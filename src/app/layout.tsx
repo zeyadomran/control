65a946551a0db70b026c6cce45019b159e9e6185
zeyadomran <ziomran@gmail.com>
2023-10-30 16:23:59 -0600

    fix css loading

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
-import './globals.css';
+import './global.css';
+import Navbar from '@/components/Navbar';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -12,7 +13,10 @@ export const metadata: Metadata = {
 const RootLayout = ({ children }: { children: React.ReactNode }) => {
 	return (
 		<html lang="en">
-			<body className={inter.className}>{children}</body>
+			<body className={inter.className}>
+				<Navbar />
+				{children}
+			</body>
 		</html>
 	);
 };
---
+++ src/components/Navbar/index.tsx
@@ -0,0 +1,11 @@
+import styles from './Navbar.module.css';
+
+const Navbar = () => {
+	return (
+		<nav className={styles.nav}>
+			<div>ddd</div>
+		</nav>
+	);
+};
+
+export default Navbar;
